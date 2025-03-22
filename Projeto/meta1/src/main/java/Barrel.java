e8e573c5dfdb65ca024d2cd758e01504afb8c7e9
Igor Reis <igor.a.c.reis7@gmail.com>
2025-03-22 16:31:23 +0000

    Added tons of files

---
+++ Projeto/meta1/src/main/java/Barrel.java
@@ -0,0 +1,49 @@
+<<<<<<< HEAD
+
+=======
+package search;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.*;
+
+public class Barrel extends UnicastRemoteObject implements Index {
+
+    private Queue<String> urlQueue;
+    private Map<String, Set<String>> invertedIndex;
+
+    protected Barrel() throws RemoteException {
+        super();
+        urlQueue = new LinkedList<>();
+        invertedIndex = new HashMap<>();
+    }
+
+    @Override
+    public synchronized String takeNext() throws RemoteException {
+        return urlQueue.poll();
+    }
+
+    @Override
+    public synchronized void putNew(String url) throws RemoteException {
+        if (!urlQueue.contains(url)) {
+            urlQueue.offer(url);
+        }
+    }
+
+    @Override
+    public synchronized void addToIndex(String word, String url) throws RemoteException {
+        invertedIndex.putIfAbsent(word, new HashSet<>());
+        invertedIndex.get(word).add(url);
+    }
+
+    @Override
+    public synchronized List<String> searchWord(String word) throws RemoteException {
+        if (invertedIndex.containsKey(word)) {
+            return new ArrayList<>(invertedIndex.get(word));
+        }
+        return Collections.emptyList();
+    }
+}
+
+i
+>>>>>>> f6db6b40f2f35b3b1ef33323f1f46127f1664f31
