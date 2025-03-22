a1bac18651f751942d328f17821ea6dad06a9aad
Miguel <miguel.p.desantos@gmail.com>
2025-03-22 16:37:28 +0000

    fixed port (777)

--- Projeto/meta1/src/main/java/search/BarrelServer.java
+++ Projeto/meta1/src/main/java/search/BarrelServer.java
@@ -9,7 +9,7 @@ public class BarrelServer {
             // Create an instance of the Barrel
             Barrel barrel = new Barrel();
             // Create (or get) an RMI registry on a specific port (e.g., 8183)
-            Registry registry = LocateRegistry.createRegistry(8183);
+            Registry registry = LocateRegistry.createRegistry(7777);
             // Bind the barrel to the registry with a unique name
             registry.rebind("barrel1", barrel);
             System.out.println("Barrel server 'barrel1' is ready.");
